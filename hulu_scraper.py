c396f6484aa736b7a2bcc2a107da7c17f393a0b1
unknown <webbdeveloper24@gmail.com>
2024-09-12 18:24:21 +0530

    Today Code

--- hulu_scraper.py
+++ hulu_scraper.py
@@ -1,3 +1,4 @@
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -6,25 +7,32 @@ from datetime import datetime, timedelta
 import csv
 import re
 import time
-import os
 
 from website_urls import NEW_ON_HULU_WEBSITE_URL
 
-def parse_date(date_str):
-    day = re.search(r'\d{1,2}', date_str)
-    if day:
-        day = int(day.group())
-        date_str = f'2024-09-{day:02d}' 
-        return datetime.strptime(date_str, '%Y-%m-%d')
-    return None
 
-def format_description(desc):
-    date_pattern = r'(Sept\.\s\d{1,2})'
-    match = re.search(date_pattern, desc)
-    if match:
-        date_str = match.group(0)
-        return desc.replace(date_str, '').strip()
-    return desc
+def convert_date(date_str, year=None):
+    month_map = {
+        'Jan.': '1', 'Feb.': '2', 'Mar.': '3', 'Apr.': '4', 'May': '5',
+        'Jun.': '6', 'Jul.': '7', 'Aug.': '8', 'Sept.': '9', 'Oct.': '10',
+        'Nov.': '11', 'Dec.': '12'}
+    
+    month_str, day = date_str.split()
+    
+    month = month_map.get(month_str, '0')  
+    if year is None:
+        year = datetime.now().year % 100
+    formatted_date = f"{month}/{day}/{year:02}"
+    return formatted_date
+
+def get_next_date(release_date_str):
+    try:
+        date = datetime.strptime(release_date_str, '%m/%d/%y')
+        next_date = date + timedelta(days=1)
+        return next_date.strftime('%m/%d/%y')
+    except ValueError as e:
+        print(f"Error parsing date '{release_date_str}': {e}")
+        return 'Invalid date format'
 
 def scrape_hulu_content():
     options = Options()
@@ -33,77 +41,47 @@ def scrape_hulu_content():
     driver = webdriver.Chrome(service=Service(), options=options)
     driver.get(NEW_ON_HULU_WEBSITE_URL)
 
-    data = []
-    heading = driver.find_element(By.XPATH, '//*[@id="c-pageArticleSingle-new-on-hulu"]/div[1]/div[1]/div[2]/div/div/h2[1]/strong').text
 
+    data = []
     try:
         time.sleep(3)
-
         firsts = driver.find_elements(By.XPATH, '//h3//strong')
         titles1 = driver.find_elements(By.XPATH, '//*[@id="c-pageArticleSingle-new-on-hulu"]/div[1]/div[1]/div[2]/div/div/p')
         image_element = driver.find_element(By.XPATH, '//*[@id="c-pageArticleSingle-new-on-hulu"]/div[1]/div[1]/div[2]/div/div/figure/div/div/picture/img')
         image_src = image_element.get_attribute('src')
-        if len(firsts) >= 5 and len(titles1) >= 10:
-            titles = titles1[5:10]
-            for first, title in zip(firsts, titles):
-                data.append([heading, first.text + ": " + title.text])
+        titles = titles1[5:10]
+        for first, title in zip(firsts, titles):
+            match = re.search(r'^(.*?)\s*\(([^)]+)\)$', first.text)
+            heading = match.group(1).strip()
+            date = match.group(2).strip()
+            current_year = datetime.now().year % 100  
+            converted_date = convert_date(date, year=current_year)
+            End_date = get_next_date(converted_date)
+            data.append([image_src, heading, title.text, 'sephora Calendar', 'All Day', 'Public', ' 0', converted_date, End_date, 'URL', '', 'Sale'])
         
-        if len(titles1) >= 38:
-            seconds1 = driver.find_elements(By.XPATH, '//*[@id="c-pageArticleSingle-new-on-hulu"]/div[1]/div[1]/div[2]/div/div/p')
-            seconds = seconds1[13:38]
-            titles = titles1[12:38]
-            i = 13
-            for second in seconds:
-                d = []
-                if i < len(titles):
-                    title = titles[i - 13].text 
-                    d.append(title)
-                data.append([heading, second.text + ": " + " ".join(d)])
-                i += 1
-
-        processed_data = []
-        for heading, description in data:
-            dates = re.findall(r'Sept\.\s\d{1,2}', description)
-            if dates:
-                for date_str in dates:
-                    start_date = parse_date(date_str)
-                    if start_date:
-                        end_date = start_date + timedelta(days=1)
-                        description_without_date = format_description(description)
-                        processed_data.append([
-                            image_src,
-                            heading,
-                            description_without_date,
-                            'sephora Calendar',
-                            'No',
-                            'Public',
-                            '0',
-                            start_date.strftime('%Y-%m-%d'),
-                            end_date.strftime('%Y-%m-%d'),
-                            NEW_ON_HULU_WEBSITE_URL,
-                            '',
-                            'Sale'
-                        ])
-                    else:
-                        print(f"Failed to parse date: {date_str}")
-
-        # Create folder if it doesn't exist
-        folder_path = 'csv_output'
-        os.makedirs(folder_path, exist_ok=True)
-        
-        # Define the path for the CSV file
-        csv_file_path = os.path.join(folder_path, 'hulu_data.csv')
-
-        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(['Image URL', 'Event Name', 'Event Description', 'Calendar', 'All Day', 'Public/Private', 'Reported Count', 'Start_Date', 'End_Date', 'URL', 'Created By', 'Event Type'])
-            csvwriter.writerows(processed_data)
+        seconds1 = driver.find_elements(By.XPATH, '//*[@id="c-pageArticleSingle-new-on-hulu"]/div[1]/div[1]/div[2]/div/div/p/strong')
+        seconds = seconds1[10:35]
+        titles = titles1[13:38]
+        for second, title in zip(seconds, titles):
+            sec = second.text
+            tt = title.text
+            date = sec.rstrip(':')
+            colon_index = tt.find(':')
+            desc = tt[colon_index + 1:].strip()
+            current_year = datetime.now().year % 100 
+            converted_date = convert_date(date, year=current_year)
+            End_date = get_next_date(converted_date)
+            data.append([image_src, '', desc, 'sephora Calendar', 'All Day', 'Public', '0', converted_date, End_date, 'URL', '', 'Sale'])
 
     except Exception as e:
         print("--------- EXCEPTION ----------------------", str(e))
 
     finally:
         driver.quit()
-
-if __name__ == "__main__":
-    scrape_hulu_content()
+        folder_path = 'csv_output'
+        os.makedirs(folder_path, exist_ok=True)  # Create folder if it doesn't exist
+        csv_file_path = os.path.join(folder_path, 'new_on_hulu.csv')
+        with open(csv_file_path, 'w', newline='', encoding='utf-8') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(['Image URL','Event Name', 'Event Description', 'Calendar', 'All Day', 'Public/Private', 'Reported Count', 'Start_Date', 'End_Date', 'URL', 'Created By', 'Event Type'])
+            csvwriter.writerows(data)
